10197f7dc714b96c3c03e8353f244606889b8ae3
nazonomikan <s25g1064yw@s.chibakoudai.jp>
2025-10-31 12:06:13 +0900

    +home

--- app5.js
+++ app5.js
@@ -4,6 +4,10 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+app.get("/", (req, res) => {
+  res.render('home');
+});
+
 app.get("/hello1", (req, res) => {
   const message1 = "Hello world";
   const message2 = "Bon jour";
