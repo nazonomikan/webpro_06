400cfc6f92e0741c05acfe1a111c68c433d3da70
nazonomikan <s25g1064yw@s.chibakoudai.jp>
2025-11-28 12:15:37 +0900

    app5jsの変更前かつ授業内容の確認済み

--- app5.js
+++ app5.js
@@ -5,6 +5,7 @@ app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 // Serve files in `public/` at the web root so `/keiyo2_add.html` works
 app.use(express.static(__dirname + "/public"));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.render('home');
